9c3dafc5a75b8c0558c4c65438549197974eaf78
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-06-01 17:16:00 +0200

    dev: use papaparse

--- tableau-periodique/main.js
+++ tableau-periodique/main.js
@@ -1,7 +1,28 @@
 console.log("Coucou");
 
-const buildPeriodicTable = () => {
+const loadData = () => {
+  return new Promise((resolve, reject) => {
+    const url = "data.csv";
+    Papa.parse(url, {
+      download: true,
+      complete: function (results) {
+        console.log("Finished:", results.data);
+        resolve(results);
+      },
+      error: function (err, file, inputElem, reason) {
+        console.log("err: ", err);
+        reject(err);
+      },
+    });
+  });
+};
+
+const buildPeriodicTable = async () => {
   console.log("start");
+
+  const csv = await loadData();
+  console.log("csv: ", csv);
+
   const div = document.querySelector("div.tableau");
   div.innerHTML =
     '<div class="element" style="transform: translate(5em, 2em);"><span>H</span></div>';
