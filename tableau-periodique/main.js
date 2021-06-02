a8c94e8953933625ad047ed4542784abbabf3bcd
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-06-02 10:20:41 +0200

    refactor: ok

--- tableau-periodique/main.js
+++ tableau-periodique/main.js
@@ -1,5 +1,3 @@
-console.log("Coucou");
-
 const loadData = () => {
   return new Promise((resolve, reject) => {
     const url = "data.csv";
@@ -7,11 +5,9 @@ const loadData = () => {
       download: true,
       header: true,
       complete: function (results) {
-        console.log("Finished:", results.data);
         resolve(results.data);
       },
       error: function (err, file, inputElem, reason) {
-        console.log("err: ", err);
         reject(err);
       },
     });
@@ -19,13 +15,10 @@ const loadData = () => {
 };
 
 const buildPeriodicTable = async () => {
-  console.log("start");
-
   const csv = await loadData();
-  console.log("csv: ", csv);
 
   const div = document.querySelector("div.tableau");
-  let str = "";
+  const array = [];
   for (const record of csv) {
     if (record.AtomicNumber === "") {
       continue;
@@ -33,16 +26,17 @@ const buildPeriodicTable = async () => {
     if (!record.Group) {
       continue;
     }
-    console.log("record: ", record);
+
     const x = 0.5 + (record.Group - 1) * 2.4;
     const y = 0.5 + (record.Period - 1) * 3.4;
-    str += `
-<div class="element" style="transform: translate(${x}em, ${y}em);">
-  <span>${record.Symbol}</span>
-</div>`;
+    const str = `
+    <div class="element" style="transform: translate(${x}em, ${y}em);">
+      <span>${record.Symbol}</span>
+    </div>`;
+    array.push(str);
   }
 
-  div.innerHTML = str;
+  div.innerHTML = array.join("");
 };
 
 addEventListener("DOMContentLoaded", buildPeriodicTable);
